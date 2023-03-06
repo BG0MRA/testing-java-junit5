623bd08e78b43724f16d1515adefa4fedf5511a9
Martin Angelov <martin.angelov02@gmail.com>
2023-03-06 19:55:06 +0200

    Chapter: Repeating Tests JUnit

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTests.java
@@ -36,7 +36,6 @@ class PersonTests implements ModelTests {
 
     @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
     @DisplayName("My repeated test")
-    @Test
     void myRepeatedTest(
             //todo - impl
 
